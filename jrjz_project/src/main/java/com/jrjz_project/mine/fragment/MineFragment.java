aa5a7bffc2bb292ac06a3502498b67930fae7904
lpsll <943394615@qq.com>
2016-08-18 17:50:43 +0800

    活动 我的布局完成 活动 我的 页面布局优化完成 我的 地址，订单跳转完成

--- jrjz_project/src/main/java/com/jrjz_project/mine/fragment/MineFragment.java
+++ jrjz_project/src/main/java/com/jrjz_project/mine/fragment/MineFragment.java
@@ -2,39 +2,50 @@ package com.jrjz_project.mine.fragment;
 
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.htlc.jrjz.jrjz_project.R;
 import com.jrjz_project.campaign.activity.TopUpActivity;
 import com.jrjz_project.common.base.BaseFragment;
+import com.jrjz_project.common.utils.ToastUtils;
+import com.jrjz_project.home.activity.SelectAddressActivity;
 import com.jrjz_project.mine.activity.ChangePhoneActivity;
 import com.jrjz_project.mine.activity.FeedBackActivity;
+import com.jrjz_project.mine.activity.MineOrderActivity;
 import com.jrjz_project.mine.activity.MoreSettingActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 我的页
  */
 public class MineFragment extends BaseFragment {
 
-    private RelativeLayout rl_mine_changephone;//更换手机号
-    private LinearLayout ll_mine_chongzhi;//充值
-    private LinearLayout ll_mine_feedback; //意见反馈
-    private LinearLayout ll_mine_more_setting ; //更多设置
+    @Bind(R.id.rl_mine_changephone)
+    RelativeLayout rlMineChangephone;
+    @Bind(R.id.ll_mine_chongzhi)
+    LinearLayout llMineChongzhi;
+    @Bind(R.id.ll_mine_more_setting)
+    LinearLayout llMineMoreSetting;
+    @Bind(R.id.ll_mine_address)
+    LinearLayout llMineAddress;
+    @Bind(R.id.ll_mine_myorder)
+    LinearLayout llMineMyorder;
+    @Bind(R.id.ll_mine_coupon)
+    LinearLayout llMineCoupon;
+    @Bind(R.id.ll_mine_feedback)
+    LinearLayout llMineFeedback;
 
     @Override
     public void initView(View view) {
 
-        rl_mine_changephone = (RelativeLayout) rootView.findViewById(R.id.rl_mine_changephone);
-        ll_mine_chongzhi = (LinearLayout) rootView.findViewById(R.id.ll_mine_chongzhi);
-        ll_mine_feedback = (LinearLayout) rootView.findViewById(R.id.ll_mine_feedback);
-        ll_mine_more_setting =(LinearLayout) rootView.findViewById(R.id.ll_mine_more_setting);
-
-        rl_mine_changephone.setOnClickListener(this);
-        ll_mine_chongzhi.setOnClickListener(this);
-        ll_mine_feedback.setOnClickListener(this);
-        ll_mine_more_setting.setOnClickListener(this);
     }
 
     @Override
@@ -53,10 +64,25 @@ public class MineFragment extends BaseFragment {
         return R.layout.fragment_mine;
     }
 
-    @Override
-    public void onClick(View v) {
 
-        switch (v.getId()) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+
+    @OnClick({R.id.rl_mine_changephone, R.id.ll_mine_chongzhi, R.id.ll_mine_more_setting,R.id.ll_mine_myorder,R.id.ll_mine_address,R.id.ll_mine_coupon,R.id.ll_mine_feedback})
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.rl_mine_changephone:
                 //跳转到切换手机号页面
                 getContext().startActivity(new Intent(getContext(), ChangePhoneActivity.class));
@@ -67,14 +93,24 @@ public class MineFragment extends BaseFragment {
                 intent.putExtra("TypeForTopUp", "WriteForUser");
                 getContext().startActivity(intent);
                 break;
-            case R.id.ll_mine_feedback:
-                //跳转到意见反馈页
-                getContext().startActivity(new Intent(getContext(), FeedBackActivity.class));
-                break;
             case R.id.ll_mine_more_setting:
                 //跳转到更多设置页
                 getContext().startActivity(new Intent(getContext(), MoreSettingActivity.class));
                 break;
+            case R.id.ll_mine_address:
+                getContext().startActivity(new Intent(getContext(), SelectAddressActivity.class));
+                break;
+            case R.id.ll_mine_myorder:
+                ToastUtils.showShort(getContext(),"wodedingdan");
+                getContext().startActivity(new Intent(getContext(), MineOrderActivity.class));
+                break;
+            case R.id.ll_mine_coupon:
+
+                break;
+            case R.id.ll_mine_feedback:
+                getContext().startActivity(new Intent(getContext(), FeedBackActivity.class));
+                break;
         }
     }
+
 }
