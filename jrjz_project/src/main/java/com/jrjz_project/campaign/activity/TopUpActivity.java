c6de6fa02dc8d389231a1409029254e177c17c17
lpsll <943394615@qq.com>
2016-08-22 12:14:38 +0800

    光标样式 图片 设置充值页面输入金额的过滤

--- jrjz_project/src/main/java/com/jrjz_project/campaign/activity/TopUpActivity.java
+++ jrjz_project/src/main/java/com/jrjz_project/campaign/activity/TopUpActivity.java
@@ -2,14 +2,19 @@ package com.jrjz_project.campaign.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.htlc.jrjz.jrjz_project.R;
 import com.jrjz_project.common.base.BaseTitleActivity;
+import com.jrjz_project.common.utils.EditInputFilter;
 import com.jrjz_project.common.utils.ToastUtils;
 
 import butterknife.Bind;
@@ -50,6 +55,8 @@ public class TopUpActivity extends BaseTitleActivity {
         cbTopupWX.setChecked(true);
         cbTopupZFB.setChecked(false);
 
+        setEditTextFilter();
+
         mIntent = getIntent();
         typeForTopUp = mIntent.getStringExtra("TypeForTopUp");
         if (!TextUtils.isEmpty(typeForTopUp)) {
@@ -87,7 +94,7 @@ public class TopUpActivity extends BaseTitleActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.tv_top_up_ok,R.id.cb_topup_weixin, R.id.cb_topup_zhifubao})
+    @OnClick({R.id.tv_top_up_ok, R.id.cb_topup_weixin, R.id.cb_topup_zhifubao})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.cb_topup_weixin:  //微信支付
@@ -102,10 +109,7 @@ public class TopUpActivity extends BaseTitleActivity {
                 //当是用户输入时，先判定输入金额是否合法
                 if ("WriteForUser".equals(typeForTopUp)) {
                     moneyForUserInput = etTopUpChongzhi.getText().toString().trim();
-                    if (TextUtils.isEmpty(moneyForUserInput) || Integer.parseInt(moneyForUserInput) == 0) {
-                        ToastUtils.showShort(TopUpActivity.this, "请输入正确金额");
-                        break;
-                    }
+                    break;
                 }
                 if (cbTopupWX.isChecked()) {
                     ToastUtils.showShort(TopUpActivity.this, "支付方式：微信支付");
@@ -122,5 +126,37 @@ public class TopUpActivity extends BaseTitleActivity {
 //                baseGoBack();
 //                break;
         }
+    }
+
+    /**
+     * 设置editText的过滤
+     */
+    private void setEditTextFilter() {
+        InputFilter[] filters = {new EditInputFilter(this)};
+        etTopUpChongzhi.setFilters(filters);
+        etTopUpChongzhi.addTextChangedListener(new TextWatcher() {   // 这是主要方法，下面为一些处理
+
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+                String str = etTopUpChongzhi.getText().toString().trim();
+                if(str.indexOf('0')==0){
+                    Toast.makeText(TopUpActivity.this, "首位不能为0", Toast.LENGTH_SHORT).show();
+                    etTopUpChongzhi.setText("");
+                }
+                if(str.indexOf('.')==0){
+                    Toast.makeText(TopUpActivity.this, "首位不能为.", Toast.LENGTH_SHORT).show();
+                    etTopUpChongzhi.setText("");
+                }
+            }
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+                // TODO Auto-generated method stub
+            }
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 }
