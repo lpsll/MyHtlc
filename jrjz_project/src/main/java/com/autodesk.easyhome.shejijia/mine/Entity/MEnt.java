d119972e9d45e6e3356b9d0a4e3874298d5a155a
lpsll <943394615@qq.com>
2016-08-31 10:22:51 +0800

    更新

---
+++ jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/mine/Entity/MEnt.java
@@ -0,0 +1,10 @@
+package com.autodesk.easyhome.shejijia.mine.entity;
+
+import com.autodesk.easyhome.shejijia.common.entity.BaseEntity;
+
+/**
+ * Created by Administrator on 2016/8/31.
+ */
+public class MEnt extends BaseEntity{
+    private String id;
+}
--- jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/mine/Entity/MineEntity.java
+++ jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/mine/Entity/MineEntity.java
@@ -7,4 +7,37 @@ import com.autodesk.easyhome.shejijia.common.entity.BaseEntity;
  */
 public class MineEntity extends BaseEntity {
     private String id;
+    private String id1;
+    private String id11;
+    private String id111;
+    private String id1111;
+    /**
+     * 获取用户信息
+     * 用户详细信息请求接口
+     * <p/>
+     * accessToken:访问授权码
+     * <p/>
+     * uid:用户ID，默认为手机号码
+     * <p/>
+     * timestamp:当前时间戳
+     * <p/>
+     * random:随机数
+     * <p/>
+     * sign:签名【生成规则uid+timestamp+random 后md5加密串】
+     */
+    /**
+     * 获取用户信息
+     * 用户详细信息请求接口
+     * <p/>
+     * accessToken:访问授权码
+     * <p/>
+     * uid:用户ID，默认为手机号码
+     * <p/>
+     * timestamp:当前时间戳
+     * <p/>
+     * random:随机数
+     * <p/>
+     * sign:签名【生成规则uid+timestamp+random 后md5加密串】
+     */
+
 }
