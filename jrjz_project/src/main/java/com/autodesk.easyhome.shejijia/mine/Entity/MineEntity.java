0fb996b786cb3200a9ee9ae5ba9b691ac1947130
lpsll <943394615@qq.com>
2016-08-31 10:24:30 +0800

    接口调试

--- jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/mine/Entity/MineEntity.java
+++ jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/mine/Entity/MineEntity.java
@@ -6,38 +6,5 @@ import com.autodesk.easyhome.shejijia.common.entity.BaseEntity;
  * Created by John_Libo on 2016/8/30.
  */
 public class MineEntity extends BaseEntity {
-    private String id;
-    private String id1;
-    private String id11;
-    private String id111;
-    private String id1111;
-    /**
-     * 获取用户信息
-     * 用户详细信息请求接口
-     * <p/>
-     * accessToken:访问授权码
-     * <p/>
-     * uid:用户ID，默认为手机号码
-     * <p/>
-     * timestamp:当前时间戳
-     * <p/>
-     * random:随机数
-     * <p/>
-     * sign:签名【生成规则uid+timestamp+random 后md5加密串】
-     */
-    /**
-     * 获取用户信息
-     * 用户详细信息请求接口
-     * <p/>
-     * accessToken:访问授权码
-     * <p/>
-     * uid:用户ID，默认为手机号码
-     * <p/>
-     * timestamp:当前时间戳
-     * <p/>
-     * random:随机数
-     * <p/>
-     * sign:签名【生成规则uid+timestamp+random 后md5加密串】
-     */
 
 }
