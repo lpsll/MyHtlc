2fee11fe5d46484d3261141efa46a98b12844dce
lpsll <943394615@qq.com>
2016-09-13 14:04:02 +0800

    页面修改

--- jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/mine/adapter/MineCouponAdapter.java
+++ jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/mine/adapter/MineCouponAdapter.java
@@ -6,6 +6,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.autodesk.easyhome.shejijia.R;
+import com.autodesk.easyhome.shejijia.common.utils.LogUtils;
 import com.autodesk.easyhome.shejijia.mine.entity.MineCouponEntity;
 import com.qluxstory.ptrrecyclerview.BaseRecyclerViewHolder;
 import com.qluxstory.ptrrecyclerview.BaseSimpleRecyclerAdapter;
@@ -31,41 +32,38 @@ public class MineCouponAdapter extends BaseSimpleRecyclerAdapter<MineCouponEntit
     public void bindData(BaseRecyclerViewHolder holder, MineCouponEntity mineCouponEntity, int position) {
         linearLayout = holder.getView(R.id.mine_coupon_lin);
         tv = holder.getView(R.id.tv_tv);
-        if(type==1){
-            if(mineCouponEntity.getAmount_level().equals("1")){
+        if (type == 1) {
+            if (mineCouponEntity.getAmount_level().equals("1")) {
                 linearLayout.setBackgroundColor(Color.parseColor("#A4E0D6"));
-            }
-            else if(mineCouponEntity.getAmount_level().equals("2")){
+            } else if (mineCouponEntity.getAmount_level().equals("2")) {
                 linearLayout.setBackgroundColor(Color.parseColor("#9EE1A4"));
-            }
-            else if(mineCouponEntity.getAmount_level().equals("3")){
+            } else if (mineCouponEntity.getAmount_level().equals("3")) {
                 linearLayout.setBackgroundColor(Color.parseColor("#FEEDA4"));
-            }
-            else if(mineCouponEntity.getAmount_level().equals("4")){
+            } else if (mineCouponEntity.getAmount_level().equals("4")) {
                 linearLayout.setBackgroundColor(Color.parseColor("#FAC18C"));
-            }
-            else if(mineCouponEntity.getAmount_level().equals("5")){
+            } else if (mineCouponEntity.getAmount_level().equals("5")) {
                 linearLayout.setBackgroundColor(Color.parseColor("#EF4E7F"));
             }
 
             tv.setVisibility(View.GONE);
-            holder.setText(R.id.tv_time,mineCouponEntity.getEffective_date().replace(" 00:00:00","")+"至"+mineCouponEntity.getExpire_date().replace(" 00:00:00",""));
-        }
-        else if(type==2){
+            holder.setText(R.id.tv_time, mineCouponEntity.getEffective_date().replace(" 00:00:00", "") + "至" + mineCouponEntity.getExpire_date().replace(" 00:00:00", ""));
+        } else if (type == 2) {
             linearLayout.setBackgroundColor(Color.parseColor("#008ce7"));
             tv.setVisibility(View.VISIBLE);
             tv.setText("已过期");
-            holder.setText(R.id.tv_time,mineCouponEntity.getExpire_date().replace(" 00:00:00","")+"已过期");
-        }
-        else if(type==3){
+            holder.setText(R.id.tv_time, mineCouponEntity.getExpire_date().replace(" 00:00:00", "") + "已过期");
+        } else if (type == 3) {
             linearLayout.setBackgroundColor(Color.parseColor("#999999"));
             tv.setVisibility(View.VISIBLE);
             tv.setText("已使用");
-            holder.setText(R.id.tv_time,mineCouponEntity.getExpire_date().replace(" 00:00:00","")+"已使用");
+            holder.setText(R.id.tv_time, mineCouponEntity.getExpire_date().replace(" 00:00:00", "") + "已使用");
         }
-        holder.setText(R.id.tv_money,mineCouponEntity.getValue_amount().replace(".00",""));
-        holder.setText(R.id.tv_sy,"满"+mineCouponEntity.getFace_amount().replace(".00","")+"元使用");
-        holder.setText(R.id.tv_jd,mineCouponEntity.getServiceName());
+        holder.setText(R.id.tv_money, mineCouponEntity.getValue_amount().replace(".00", ""));
+        holder.setText(R.id.tv_sy, "满" + mineCouponEntity.getFace_amount().replace(".00", "") + "元使用");
+
+
+        LogUtils.e("有效期==========" + mineCouponEntity.getServiceName());
+        holder.setText(R.id.tv_jd, mineCouponEntity.getServiceName());
 
     }
 }
--- jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/order/adapter/CouponAdapter.java
+++ jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/order/adapter/CouponAdapter.java
@@ -1,22 +1,13 @@
 package com.autodesk.easyhome.shejijia.order.adapter;
 
-import android.content.Context;
 import android.graphics.Color;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.autodesk.easyhome.shejijia.R;
-import com.autodesk.easyhome.shejijia.order.entity.OrderEntity;
+import com.autodesk.easyhome.shejijia.common.utils.LogUtils;
 import com.autodesk.easyhome.shejijia.order.entity.ServiceCouponEntity;
 import com.qluxstory.ptrrecyclerview.BaseRecyclerViewHolder;
 import com.qluxstory.ptrrecyclerview.BaseSimpleRecyclerAdapter;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by John_Libo on 2016/8/18.
@@ -51,6 +42,7 @@ public class CouponAdapter extends BaseSimpleRecyclerAdapter<ServiceCouponEntity
         holder.setText(R.id.tv_money,serviceCouponEntity.getValue_amount().replace(".00",""));
         holder.setText(R.id.tv_sy,"满"+serviceCouponEntity.getFace_amount().replace(".00","")+"元使用");
         holder.setText(R.id.tv_jd,serviceCouponEntity.getServiceName());
+        LogUtils.d("有效期====="+serviceCouponEntity.getEffective_date());
         holder.setText(R.id.tv_time,serviceCouponEntity.getEffective_date().replace(" 00:00:00","")+"至"+serviceCouponEntity.getExpire_date().replace(" 00:00:00",""));
 
     }
