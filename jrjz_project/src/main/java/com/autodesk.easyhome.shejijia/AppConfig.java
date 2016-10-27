791f252b928ea162a8b2e54ad8fb4b17f1462e6d
lpsll <943394615@qq.com>
2016-10-27 18:58:41 +0800

    更新

--- jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/AppConfig.java
+++ jrjz_project/src/main/java/com/autodesk.easyhome.shejijia/AppConfig.java
@@ -28,15 +28,18 @@ public class AppConfig {
     public static final String NOTHING = "0";
     //测试环境
 //    public static final String BASE_URL = "http://101.200.167.130:8080/jrjz-api/api";
-    //正式环境
-    public static final String BASE_URL = "http://223.72.146.122:8080/jrjz-api/api";
-    public static final String BASE_IMG_URL = "http://223.72.146.122:8080/jrjz-api";
+    //正式环境（ip）
+//    public static final String BASE_URL = "http://223.72.146.122:8080/jrjz-api/api";
+    //正式环境（域名）
+    public static final String BASE_URL = "http://jrjz.juran.com.cn/jrjz-api/api";
+    public static final String BASE_IMG_URL = "http://jrjz.juran.com.cn/jrjz-api";
     //    public static final String ABOUT_JURAN = "http://mp.weixin.qq.com/s?__biz=MzIxNDM4OTY3Mw==&mid=100000015&idx=1&sn=8f967c4b219ab2dc69400489b5d1b6b0&scene=18#wechat_redirect";
-    public static final String ABOUT_JURAN = "http://223.72.146.122:8080/jrjz-api/static/html/AboutUs.html";
+    public static final String ABOUT_JURAN = "http://jrjz.juran.com.cn/jrjz-api/static/html/AboutUs.html";
     public static final String PROTOCOL = "file:///android_asset/Protocol.html";
     //下载链接
     public static final String DOWNLOAD = "http://a.app.qq.com/o/simple.jsp?pkgname=com.autodesk.easyhome.shejijia";
-    public static final String DOWNLOAD_H5 = "http://cuohe.damaimob.com/JuRanCode/index.html";
+    public static final String DOWNLOAD_H5 = "http://jrjz.juran.com.cn/JuRanCode/index.html";
+//    public static final String DOWNLOAD_H5 = "http://cuohe.damaimob.com/JuRanCode/index.html";
 
 
     public static final String Wx_App_Id = "wxfd04ee1c78a46319";
